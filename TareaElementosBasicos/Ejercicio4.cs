d120aa8f11c15c7b33c154f911e7f42bd7b35c98
Fatima Ayala <ftayala89@gmail.com>
2021-08-03 15:27:01 -0600

    ejercicio4

---
+++ TareaElementosBasicos/Ejercicio4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaElementosBasicos
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            //Se declaran las variables
+            double cent, far;
+
+            //Se ingresan los datos
+            Console.WriteLine("Ingrese los grados que convertir: ");
+            cent = Convert.ToDouble(Console.ReadLine());
+
+            //Se hace la conversion 
+            far = (9 / 5) * cent + 32;
+
+            // Salida de los datos
+            Console.WriteLine("En grados Fahrenheit sus grados centigrados son: {0} ", far);
+
+            Console.ReadKey();
+        }
+
+       
+
+          
+
+    }
+}
